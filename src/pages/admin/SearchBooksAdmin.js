b35974d5535352e330804fdd85676359c8141fbf
JohnPaulPacatang <johnpaulpacatang1gmail.com>
2024-03-20 17:01:09 +0800

    admin books

--- src/pages/admin/SearchBooksAdmin.js
+++ src/pages/admin/SearchBooksAdmin.js
@@ -3,12 +3,6 @@ import { supabase } from '../../utils/supabaseClient';
 import { BiSearch } from "react-icons/bi";
 
 const AdminSearchBooks = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-
   // Dropdown category and search
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("All");
@@ -23,7 +17,6 @@ const AdminSearchBooks = () => {
   ];
 
   const [books, setBooks] = useState([]);
-  const [book, setBook] = useState([]);
 
   useEffect(() => {
     fetchBooks();
@@ -34,44 +27,6 @@ const AdminSearchBooks = () => {
     setBooks(data);
   }
 
-  function handleChange(event) {
-    setBook((prevFormData) => ({
-      ...prevFormData,
-      [event.target.name]: event.target.value,
-    }));
-  }
-
-  async function addBook() {
-    try {
-      console.log("Adding books...");
-      await supabase
-        .from('books')
-        .insert([
-          {
-            ddc_id: book.ddcId,
-            title: book.title,
-            author: book.author,
-            category: book.category,
-            availability: book.availability === 'true' ? true : false,
-          },
-        ]);
-
-      setShowModal(false);
-      console.log("Book added successfully.");
-      fetchBooks();
-
-      setBook({
-        ddcId: '',
-        title: '',
-        author: '',
-        category: '',
-        availability: '',
-      });
-
-    } catch (error) {
-      console.error("Error adding book:", error);
-    }
-  }
 
   // Dropdown category and search
   const filteredData = books.filter((book) =>
@@ -121,12 +76,6 @@ const AdminSearchBooks = () => {
               <th colSpan="10">
                 <div className="flex justify-between items-center px-5 py-4">
                   <h2 className="text-xl text-black">Book list</h2>
-                  <button
-                    className="bg-white text-black border rounded-xl p-3 hover:bg-maroon hover:text-white"
-                    onClick={handleOpenModal}
-                  >
-                    Add Book
-                  </button>
                 </div>
               </th>
             </tr>
@@ -156,91 +105,6 @@ const AdminSearchBooks = () => {
           </tbody>
         </table>
       </div>
-
-      {showModal && (
-        <div className="fixed inset-0 z-10 flex justify-center items-center shadow-2xl" onClick={() => setShowModal(false)} >
-          <div className="bg-peach p-8 rounded-lg shadow-2xl" onClick={(e) => e.stopPropagation()}>
-            <h2 className="text-2xl font-bold mb-4 text-center">
-              Book Information
-            </h2>
-
-            <form onSubmit={(e) => { e.preventDefault(); addBook(book); }}>
-              <div className="grid grid-cols-2 gap-10">
-                <div className="flex flex-col w-72">
-                  <label className="text-sm ml-1 font-semibold">DDC ID</label>
-                  <input
-                    type="number"
-                    step="any"
-                    placeholder="DDC ID"
-                    className="shadow-lg rounded-xl text-sm px-5 py-4 mb-4 w-full"
-                    name="ddcId"
-                    value={book.ddcId}
-                    onChange={handleChange}
-                    required
-                  />
-
-                  <label className="text-sm ml-1 font-semibold">Title</label>
-                  <input
-                    type="text"
-                    placeholder="Title"
-                    className="shadow-lg rounded-xl text-sm px-5 py-4 mb-4 w-full"
-                    name="title"
-                    value={book.title}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-
-                <div className="flex flex-col w-72">
-                  <label className="text-sm ml-1 font-semibold">Author</label>
-                  <input
-                    type="text"
-                    placeholder="Author"
-                    className="shadow-lg rounded-xl text-sm px-5 py-4 mb-4 w-full"
-                    name="author"
-                    value={book.author}
-                    onChange={handleChange}
-                    required
-                  />
-
-                  <label className="text-sm ml-1 font-semibold">Category</label>
-                  <input
-                    type="text"
-                    placeholder="Category"
-                    className="shadow-lg rounded-xl text-sm px-5 py-4 mb-4 w-full"
-                    name="category"
-                    value={book.category}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-              </div>
-
-              <div className="flex justify-center">
-                <div className="flex flex-col">
-                  <label className="text-sm ml-1 font-semibold">Availability</label>
-                  <select
-                    className="shadow-lg rounded-xl text-sm px-5 py-4 mb-4 font-semibold"
-                    name="availability"
-                    value={book.availability}
-                    onChange={handleChange}
-                    required>
-                    <option value="">Select Status</option>
-                    <option value={true}>TRUE</option>
-                    <option value={false}>FALSE</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className="flex justify-center pt-4">
-                <button type="submit" className="bg-maroon text-white py-2 px-4 rounded-lg mr-2 hover:bg-blue" >
-                  Add Book
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
