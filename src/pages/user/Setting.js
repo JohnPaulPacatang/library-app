2786465af544e5bea7a99cdb81a38ebfacab0c49
JohnPaulPacatang <johnpaulpacatang1gmail.com>
2024-03-27 17:49:28 +0800

    minor changes

--- src/pages/user/Setting.js
+++ src/pages/user/Setting.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Swal from "sweetalert2";
 import Profile from "../../assets/profile.jpg";
 
 const Setting = ({ userFirstName, userLastName, userMiddleName, userEmail, userCourse }) => {
@@ -11,31 +12,14 @@ const Setting = ({ userFirstName, userLastName, userMiddleName, userEmail, userC
     setShowModal(true);
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
-
-    setCurrentPassword("");
-    setNewPassword("");
-    setPassword("");
-  };
-
-  const handleResetPassword = () => {
-    console.log("Resetting password...");
-    handleCloseModal();
-  };
- 
-
   return (
     <div className="flex-1 px-5">
       <div className="flex justify-center items-center bg-white shadow my-24 mx-3 rounded-xl h-3/4">
         <div className="m-auto p-10 mr-4">
           <ul className="list-unstyled">
             <li className="mb-2 flex flex-col items-center">
-              <img src={Profile} alt="User profile" height={300} width={300}/>
-              <button
-                className="text-blue font-semibold hover:underline mt-2"
-                onClick={handleOpenModal}
-              >
+              <img src={Profile} alt="User profile" height={300} width={300} />
+              <button className="text-blue-600 hover:underline mt-2 font-semibold hover:text-blue"onClick={handleOpenModal}>
                 Reset password
               </button>
             </li>
@@ -64,7 +48,7 @@ const Setting = ({ userFirstName, userLastName, userMiddleName, userEmail, userC
                 Last Name:
               </label>
               <p className="text-black bg-gray p-3 rounded-lg w-4/5">
-               {userLastName}
+                {userLastName}
               </p>
             </div>
             <div className="mb-4">
@@ -74,7 +58,7 @@ const Setting = ({ userFirstName, userLastName, userMiddleName, userEmail, userC
             <div className="mb-4">
               <label className="block text-black font-bold mb-2">Email:</label>
               <p className="text-black bg-gray p-3 rounded-lg w-4/5">
-               {userEmail}
+                {userEmail}
               </p>
             </div>
           </div>
@@ -87,47 +71,44 @@ const Setting = ({ userFirstName, userLastName, userMiddleName, userEmail, userC
           <div className="bg-white p-6 rounded-lg">
             <h2 className="text-lg font-bold mb-4">Reset Password</h2>
 
-            <label className="text-sm ml-1 font-semibold">Current password:</label>
-            <input
-              type="password"
-              placeholder="Current Password"
-              className="rounded-lg input-border shadow px-3 py-2 mb-4 w-full"
-              value={currentPassword}
-              onChange={(e) => setCurrentPassword(e.target.value)}
-            />
+            <form>
+              <label className="text-sm ml-1 font-semibold">Current password:</label>
+              <input
+                type="password"
+                placeholder="Current Password"
+                className="rounded-lg input-border shadow px-3 py-2 mb-4 w-full"
+                value={currentPassword}
+                onChange={(e) => setCurrentPassword(e.target.value)}
+              />
 
-            <label className="text-sm ml-1 font-semibold">New password:</label>
-            <input
-              type="password"
-              placeholder="New Password"
-              className="rounded-lg input-border shadow px-3 py-2 mb-4 w-full"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
+              <label className="text-sm ml-1 font-semibold">New password:</label>
+              <input
+                type="password"
+                placeholder="New Password"
+                className="rounded-lg input-border shadow px-3 py-2 mb-4 w-full"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
 
-            <label className="text-sm ml-1 font-semibold">Re-type password:</label>
-            <input
-              type="password"
-              placeholder="Re-type Password"
-              className="rounded-lg input-border shadow px-3 py-2 mb-4 w-full"
-              value={Password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+              <label className="text-sm ml-1 font-semibold">Re-type password:</label>
+              <input
+                type="password"
+                placeholder="Re-type Password"
+                className="rounded-lg input-border shadow px-3 py-2 mb-4 w-full"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
 
-            <div className="flex justify-end">
-              <button
-                className="bg-blue-600 text-white py-2 px-4 rounded mr-2 bg-blue"
-                onClick={handleResetPassword}
-              >
-                Confirm
-              </button>
-              <button
-                className="bg-gray-300 text-gray-800 py-2 px-4 rounded bg-gray"
-                onClick={handleCloseModal}
-              >
-                Cancel
-              </button>
-            </div>
+              <div className="flex justify-end">
+                <button type="submit" className="bg-blue-600 text-white py-2 px-4 rounded mr-2 bg-blue">
+                  Confirm
+                </button>
+                <button className="bg-gray-300 text-gray-800 py-2 px-4 rounded bg-gray" onClick={() => setShowModal(false)}>
+                  Cancel
+                </button>
+              </div>
+            </form>
+
           </div>
         </div>
       )}
